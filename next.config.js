cb28704c09b04b6c7288f88689cdbd905d9215ee
Mustafa CEYLAN <mceylan6634@gmail.com>
2024-12-27 11:40:55 +0300

    Update: Change server port to 3001 and fix build issues

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  output: 'standalone',
+  images: {
+    domains: ['blog.mustafaceylan.tech'],
+    unoptimized: true,
+  }
 }
 
 module.exports = nextConfig 
